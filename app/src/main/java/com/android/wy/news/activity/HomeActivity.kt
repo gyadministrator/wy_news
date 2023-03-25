e7864b2fd3c56a15f05ed6ba4a09c38f0d43cc8b
gy <1562211757@qq.com>
2023-03-25 21:58:31 +0800

    添加圆角头像

--- app/src/main/java/com/android/wy/news/activity/HomeActivity.kt
+++ app/src/main/java/com/android/wy/news/activity/HomeActivity.kt
@@ -102,8 +102,7 @@ class HomeActivity : GYBottomActivity(), GYBottomBarView.IGYBottomBarChangeListe
     override fun onSelected(position: Int) {
         if (position == 2) {
             hideSearch()
-            ImmersionBar.with(this).statusBarColor(R.color.black)
-                .navigationBarColor(R.color.black).statusBarDarkFont(false).init()
+            ImmersionBar.with(this).statusBarColor(R.color.black).statusBarDarkFont(false).init()
         } else {
             showSearch()
             ImmersionBar.with(this).statusBarColor(R.color.status_bar_color)
